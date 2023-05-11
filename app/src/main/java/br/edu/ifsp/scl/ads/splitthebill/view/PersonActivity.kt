1a830c0b39a0816f401a20c835d5ff8995b64dff
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-05-10 23:43:39 -0300

    fix: Fixed PersonActivity binding

--- app/src/main/java/br/edu/ifsp/scl/ads/splitthebill/view/PersonActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/splitthebill/view/PersonActivity.kt
@@ -2,11 +2,11 @@ package br.edu.ifsp.scl.ads.splitthebill.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.edu.ifsp.scl.ads.splitthebill.databinding.ActivityMainBinding
+import br.edu.ifsp.scl.ads.splitthebill.databinding.ActivityPersonBinding
 
 class PersonActivity : AppCompatActivity() {
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+    private val binding: ActivityPersonBinding by lazy {
+        ActivityPersonBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
