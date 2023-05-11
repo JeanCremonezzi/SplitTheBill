c908e1b07feb399f413706c696302b30acb8c036
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-05-11 00:02:07 -0300

    feat: Add functionality to clear add person fields

--- app/src/main/java/br/edu/ifsp/scl/ads/splitthebill/view/PersonActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/splitthebill/view/PersonActivity.kt
@@ -1,7 +1,11 @@
 package br.edu.ifsp.scl.ads.splitthebill.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import br.edu.ifsp.scl.ads.splitthebill.R
 import br.edu.ifsp.scl.ads.splitthebill.databinding.ActivityPersonBinding
 
 class PersonActivity : AppCompatActivity() {
@@ -12,5 +16,25 @@ class PersonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean{
+        menuInflater.inflate(R.menu.person_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.clearPerson -> {
+                with (binding) {
+                    nameEt.setText("")
+                    valueEt.setText("")
+                    itemsEt.setText("")
+                }
+
+                true
+            }
+            else -> false
+        }
     }
 }
