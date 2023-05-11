66664936acf9225022a673afc56285a7ccb1027d
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-05-10 22:15:56 -0300

    feat: Add action bar icons

--- app/src/main/java/br/edu/ifsp/scl/ads/splitthebill/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/splitthebill/MainActivity.kt
@@ -2,6 +2,7 @@ package br.edu.ifsp.scl.ads.splitthebill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import br.edu.ifsp.scl.ads.splitthebill.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean{
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
