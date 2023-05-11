15f80d02eee2bbb7939dcf0c62d4ad0cad2b86d4
JeanCremonezzi <jeancremonezzidev@gmail.com>
2023-05-10 22:10:20 -0300

    build: Configured SDK and view binding

--- app/src/main/java/br/edu/ifsp/scl/ads/splitthebill/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/splitthebill/MainActivity.kt
@@ -2,10 +2,15 @@ package br.edu.ifsp.scl.ads.splitthebill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.ifsp.scl.ads.splitthebill.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
